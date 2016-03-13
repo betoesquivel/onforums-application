a9633847e315233b9fe9041ee10c47bd8e9a7cac
Beto Esquivel <ppbeto94@gmail.com>
2016-03-13 00:32:24 +0000

    Creates flask app, runs scraper, but don't know if it is running. Installs pymongo.

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from flask import Flask
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scraper.guardianukscraper.spiders.guardian_spider import GuardianSpider
+app = Flask(__name__)
+
+process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+})
+@app.route("/")
+def hello():
+    process.crawl (GuardianSpider)
+    process.start()
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
